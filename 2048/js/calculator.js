3ba1189199f468fb6041d53f0eeea28fc3bae58b
zifeng <987400803@qq.com>
2016-06-23 10:08:36 +0800

    add addtional image to decorate the sweetalert

--- 2048/js/calculator.js
+++ 2048/js/calculator.js
@@ -1,6 +1,55 @@
 var calc = (function calc(){
 	// 记录最新解锁的英雄
 	var max = 0;
+
+	var herolist = {
+		1:{
+			// description:,
+			imageUrl:'Teemo.png'
+		},
+		2:{
+			// description:,
+			imageUrl:'pbe0307_02.png'
+		},
+		3:{
+			// description:,
+			imageUrl:'Leona_Square_0.png'
+		},
+		4:{
+			// description:,
+			imageUrl:'LeeSin_Square_0.png'
+		},
+		5:{
+			// description:,
+			imageUrl:'Thresh_Square_0.png'
+		},
+		6:{
+			// description:,
+			imageUrl:'1385536285032.png'
+		},
+		7:{
+			// description:,
+			imageUrl:'Pantheon_Square_0.png'
+		},
+		8:{
+			// description:,
+			imageUrl:'LuLu_Square_0.png'
+		},
+		9:{
+			// description:,
+			imageUrl:'MonkeyKing_Square_0.png'
+		},
+		10:{
+			// description:,
+			imageUrl:'Khazix_Square_0.png'
+		},
+		11:{
+			// description:,
+			imageUrl:'Vi_Square_0.png'
+		}
+	}
+
+
 	return function (){
 		// 获取英雄的权值列表
 		var list = document.getElementsByClassName('tile-inner');
@@ -21,7 +70,8 @@ var calc = (function calc(){
 			// alert('解锁了第'+substractBy2(max)+'个英雄！');
 			swal({
 				title:'Congratulation',
-				text:'恭喜你成功解锁第'+substractBy2(max)+'个英雄！fighting...'
+				text:'恭喜你解锁了第'+substractBy2(max)+'个英雄！继续努力！！！',
+				imageUrl:'img/'+herolist[substractBy2(max)].imageUrl
 			});
 		}
 	}
